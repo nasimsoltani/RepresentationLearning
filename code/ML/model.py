8c0f15e993fa244eae345af732dc1663040995b5
nasimsoltani <nasimsoltani67@gmail.com>
2025-06-04 14:54:48 -0500

    rf fingerprint model added

--- code/ML/model.py
+++ code/ML/model.py
@@ -4,6 +4,50 @@ import torch.nn as nn
 import torch.nn as nn
 import numpy as np
 
+
+class RFFingerprintingNet(nn.Module):
+	""" input shape needs to be (b, 2, 1024) """
+
+	def __init__(self, slice_size, num_classes):
+		
+		super(RFFingerprintingNet, self).__init__()
+		
+		dropProb = 0.25
+		channel = 64 
+		self.conv0 = nn.Conv1d(2, channel, kernel_size=7, padding="same")
+		self.conv1 = nn.Conv1d(channel, channel, kernel_size=7, padding="same")
+		self.conv2 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
+		self.pool1 = nn.MaxPool1d(2,2)
+		self.flatten = nn.Flatten()
+		self.hidden1 = nn.Linear(int(slice_size*2), 256) 
+		self.hidden2 = nn.Linear(256, 128)
+		self.out = nn.Linear(128, num_classes)
+		self.drop = nn.Dropout(dropProb)
+		self.relu = nn.ReLU()
+
+	def forward(self, x):
+		
+		x = self.relu(self.conv0(x))
+		x = self.relu(self.conv2(x))
+		x = self.pool1(x)
+
+		for _ in range(4):
+			
+			x = self.relu(self.conv1(x))
+			x = self.relu(self.conv2(x))
+			x = self.pool1(x)
+
+		x = self.flatten(x)
+		x = self.drop(x)
+		x = self.relu(self.hidden1(x))
+		x = self.drop(x)
+		x = self.relu(self.hidden2(x))
+		x = self.drop(x)
+		x = self.out(x)
+		
+		return x
+
+
 class IQNet(nn.Module):
 	""" input shape needs to be (b, 2, 1024) """
 
@@ -117,12 +161,11 @@ class SpectrogramNet(nn.Module):
 
 if __name__ == '__main__':
 
-	slice_size = 2048 
-	num_classes = 8
-	num_channels = 3
+	slice_size = 1024 
+	num_classes = 16
 
 	# create the model:
-	model = SpectrogramNet(slice_size, num_channels, num_classes)
+	model = RFFingerprintingNet(slice_size, num_classes)
 
 	# print number of parameters in the model
 	pp=0
@@ -134,10 +177,7 @@ if __name__ == '__main__':
 	print('This model has ' +str(pp)+ ' parameters')
 
 	input = torch.rand((1, 2, slice_size))
-	input = torch.rand((1, 3,300,200))
-	output_x, output_feature = model(input)
+	
+	output = model(input)
 
-	print(output_feature.size)
-
-	print(output_x.shape)
-	print(output_feature.shape)
+	print(output.shape)
