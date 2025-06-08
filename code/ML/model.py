6d7ad7e1b581a0febba95411a4952aadeac8bb19
nasimsoltani <nasimsoltani67@gmail.com>
2025-06-08 10:24:02 -0500

    NN revised

--- code/ML/model.py
+++ code/ML/model.py
@@ -16,8 +16,16 @@ class RFFingerprintingNet(nn.Module):
 		if not self.mtl:
 			channel = 64
 			self.conv0 = nn.Conv1d(2, channel, kernel_size=7, padding="same")
-			self.conv1 = nn.Conv1d(channel, channel, kernel_size=7, padding="same")
-			self.conv2 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
+			self.conv1 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
+			self.conv2 = nn.Conv1d(channel, channel, kernel_size=7, padding="same")
+			self.conv3 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
+			self.conv4 = nn.Conv1d(channel, channel, kernel_size=7, padding="same")
+			self.conv5 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
+			self.conv6 = nn.Conv1d(channel, channel, kernel_size=7, padding="same")
+			self.conv7 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
+			self.conv8 = nn.Conv1d(channel, channel, kernel_size=7, padding="same")
+			self.conv9 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
+			
 			self.pool1 = nn.MaxPool1d(2,2)
 			self.flatten = nn.Flatten()
 			self.relu = nn.ReLU()
@@ -43,128 +51,28 @@ class RFFingerprintingNet(nn.Module):
 			features = common_rep
 		else:
 			x = self.relu(self.conv0(x))
+			x = self.relu(self.conv1(x))
+			x = self.pool1(x)
+		
 			x = self.relu(self.conv2(x))
+			x = self.relu(self.conv3(x))
+			x = self.pool1(x)
+			
+			x = self.relu(self.conv4(x))
+			x = self.relu(self.conv5(x))
 			x = self.pool1(x)
 
-			for _ in range(4):
-				x = self.relu(self.conv1(x))
-				x = self.relu(self.conv2(x))
-				x = self.pool1(x)
+			x = self.relu(self.conv6(x))
+			x = self.relu(self.conv7(x))
+			x = self.pool1(x)
 			
+			x = self.relu(self.conv8(x))
+			x = self.relu(self.conv9(x))
+			x = self.pool1(x)
+		
 			features = self.flatten(x)
 		
 		return self.classifier(features)
-
-
-class IQNet(nn.Module):
-	""" input shape needs to be (b, 2, 1024) """
-
-	def __init__(self, slice_size, num_channels, num_classes):
-		super(IQNet, self).__init__()
-		dropProb = 0.25
-		channel = 64  # original: 32
-		self.conv1 = nn.Conv1d(num_channels, channel, kernel_size=7, padding="same")
-		self.conv2 = nn.Conv1d(channel, channel, kernel_size=5, padding="same")
-		self.pool1 = nn.MaxPool1d(2,2)
-		self.pool2 = nn.MaxPool1d(3, 2, padding=1)
-		self.flatten = nn.Flatten()
-		self.hidden1 = nn.Linear(int(slice_size*8), 512) #original slice_size*0.5   
-		self.hidden2 = nn.Linear(512, 256)
-		self.out = nn.Linear(256, num_classes)
-		self.drop = nn.Dropout(dropProb)
-		self.relu = nn.ReLU()
-		self.softmax = nn.Softmax()
-
-	def forward(self, x):
-		x = self.relu(self.conv1(x))
-
-		b = x
-		x = self.relu(self.conv2(x))
-		x = self.relu(self.conv2(x))
-		x = torch.add(x, b)
-		x = self.pool1(x)
-		c = x = self.drop(x)
-		
-
-		x = self.relu(self.conv2(x))
-		x = self.relu(self.conv2(x))
-		x = torch.add(x, c)
-		x = self.pool2(x)
-		d = x = self.drop(x)
-	
-		
-		x = self.relu(self.conv2(x))
-		x = self.relu(self.conv2(x))
-		x = torch.add(x, d)
-		x = self.pool2(x)
-		x = self.drop(x)
-        
-		x = self.flatten(x)
-		x = self.relu(self.hidden1(x))
-		x = self.drop(x)
-		
-		feature = x
-		
-		x = self.relu(self.hidden2(x))
-		x = self.drop(x)
-		x = self.out(x)  # no softmax: CrossEntropyLoss()
-		return feature, x
-
-
-
-class SpectrogramNet(nn.Module):
-	""" input shape needs to be (b, 3, 90, 160) """
-
-	def __init__(self, slice_size, num_channels, num_classes):
-		super(SpectrogramNet, self).__init__()
-		dropProb = 0.25
-		channel = 32  # original: 32
-		self.conv1 = nn.Conv2d(num_channels, channel, kernel_size=(7,7), padding="same")
-		self.conv2 = nn.Conv2d(channel, channel, kernel_size=(5,5), padding="same")
-		self.pool1 = nn.MaxPool2d((2,2))
-		self.pool2 = nn.MaxPool2d((3,3), padding=1)
-		self.flatten = nn.Flatten()
-		self.hidden1 = nn.Linear(6528, 512) #original slice_size*0.5   
-		self.hidden2 = nn.Linear(512, 256)
-		self.out = nn.Linear(256, num_classes)
-		self.drop = nn.Dropout(dropProb)
-		self.relu = nn.ReLU()
-		self.softmax = nn.Softmax()
-
-	def forward(self, x):
-		x = self.relu(self.conv1(x))
-
-		b = x
-		x = self.relu(self.conv2(x))
-		x = self.relu(self.conv2(x))
-		x = torch.add(x, b)
-		x = self.pool1(x)
-		c = x = self.drop(x)
-		
-
-		x = self.relu(self.conv2(x))
-		x = self.relu(self.conv2(x))
-		x = torch.add(x, c)
-		x = self.pool2(x)
-		d = x = self.drop(x)
-	
-		
-		x = self.relu(self.conv2(x))
-		x = self.relu(self.conv2(x))
-		x = torch.add(x, d)
-		x = self.pool2(x)
-		x = self.drop(x)
-        
-		x = self.flatten(x)
-		x = self.relu(self.hidden1(x))
-		x = self.drop(x)
-		
-		feature = x
-		
-		x = self.relu(self.hidden2(x))
-		x = self.drop(x)
-		x = self.out(x)  # no softmax: CrossEntropyLoss()
-		return feature, x
 
 
 if __name__ == '__main__':
